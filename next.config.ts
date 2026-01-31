305cb5ee2c56a4c48689eb4b617e9350f6cd0e73
saqib-7 <saqibchamediya07@gmail.com>
2026-01-31 22:48:36 +0530

    modified next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 export default nextConfig;
